12e8340ee58e9fd6d790e65e1700fe3b0ec37ea0
gurezo <akihiko.kigure@gmail.com>
2017-01-02 10:06:45 +0900

    panelコンポーネント実装メモ程度

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,19 +1,21 @@
 /*  TapGameのゲームページ用パーツコンポーネント  */
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
-export class PanelComponent implements OnInit {
+export class PanelComponent implements DoCheck {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
   //タップ時画像入れ替えメソッド
   //memo NgClass,NgStyleで可能かも。
   //https://is.gd/VqJuQ4
+  ngDoCheck(){
+    //htmlタグで書いた方がスマートだけど
+    //チェック処理は、tsファイルに実装した方が良さそう
+  }
+
 }
