708074a0e2726aa20a0235c4782af506d4c243c6
gurezo <akihiko.kigure@gmail.com>
2017-01-07 17:46:04 +0900

    画面遷移修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-   <h1>テスト</h1>
    <nav>
 <!--
 デバッグ用
+<a routerLink="/game">game</a>
 -->
     <a routerLink="/top">top</a>
-    <a routerLink="/game">game</a>
-     <a routerLink="/clear">clear</a>
+    <a routerLink="/clear">clear</a>
    </nav>
    <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = '暇潰し';
 }
