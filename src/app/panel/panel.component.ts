ac18f05df7aadeaa586dc4f132b23a0f7a126365
gurezo <akihiko.kigure@gmail.com>
2017-01-08 10:15:34 +0900

    CSS、テンプレートの簡素化

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -13,10 +13,14 @@ export class PanelComponent implements DoCheck {
   //タップ時画像入れ替えメソッド
   //memo NgClass,NgStyleで可能かも。
   //https://is.gd/VqJuQ4
-  ngDoCheck(){
+  ngDoCheck() {
     //htmlタグで書いた方がスマートだけど
     //チェック処理は、tsファイルに実装した方が良さそう
     // 画像のソース指定を切り替える方式にする
   }
 
+  // タップした時の処理
+  onTap(): void {
+    //配列のフラグを帰るか、スタイルで判定するか・・・・。
+  }
 }
