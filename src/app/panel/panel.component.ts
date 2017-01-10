d20f87aa2869d60c779caa70f271a55dbd34ed17
gurezo <akihiko.kigure@gmail.com>
2017-01-10 11:06:48 +0900

    タップ処理実装変更

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,26 +1,29 @@
 /*  TapGameのゲームページ用パーツコンポーネント  */
-import { Component, DoCheck } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
-export class PanelComponent implements DoCheck {
+export class PanelComponent implements OnInit {
 
   constructor() { }
 
-  //タップ時画像入れ替えメソッド
-  //memo NgClass,NgStyleで可能かも。
-  //https://is.gd/VqJuQ4
-  ngDoCheck() {
+  tapflg;
+
+  ngOnInit() {
     //htmlタグで書いた方がスマートだけど
     //チェック処理は、tsファイルに実装した方が良さそう
     // 画像のソース指定を切り替える方式にする
+    this.tapflg = true;
   }
 
   // タップした時の処理
   onTap(): void {
     //配列のフラグを帰るか、スタイルで判定するか・・・・。
+    if (this.tapflg) {
+      this.tapflg = false;
+    }
   }
 }
